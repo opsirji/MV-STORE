ab2493db2392a62ffc8de6461157c1c521f115a9
opsirji <137800473+opsirji@users.noreply.github.com>
2023-06-26 23:04:55 +0530

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -10,7 +10,7 @@ async def cb_handler(client: Bot, query: CallbackQuery):
     data = query.data
     if data == "about":
         await query.message.edit_text(
-            text = f"<b>○ Creator : <a href='tg://user?id={OWNER_ID}'>This Person</a>\n○ Language : <code>Python3</code>\n○ Library : <a href='https://docs.pyrogram.org/'>Pyrogram asyncio {__version__}</a>\n○ Source Code : <a href='https://github.com/CodeXBotz/File-Sharing-Bot'>Click here</a>\n○ Channel : @CodeXBotz\n○ Support Group : @CodeXBotzSupport</b>",
+            text = f"<b>ᴅᴇᴠᴇʟᴏᴘᴇʀ:- <a href='tg://user?id={OWNER_ID}'>ꜱᴛᴀʀ ʙᴏʏ</a>\nʟᴀɴɢᴜᴀɢᴇ:- <code>ᴘʏᴛʜᴏɴ 3</code>\nʟɪʙʀᴀʀʏ:-<a href='https://docs.pyrogram.org/'>Pyrogram asyncio {__version__}</a>\nᴜᴘᴅᴀᴛᴇꜱ ᴄʜᴀɴɴᴇʟ:- <a href='https://t.me/mv_updates1'>ᴍᴠ ᴜᴘᴅᴀᴛᴇꜱ</a>\nᴍᴀɪɴ ᴄʜᴀɴɴᴇʟ:- <a href='https://t.me/hdmaxx'></a>\nʀᴇǫᴜᴇꜱᴛ ɢʀᴏᴜᴘ :- <a href='https://t.me/hd_request'></a></b>",
             disable_web_page_preview = True,
             reply_markup = InlineKeyboardMarkup(
                 [
