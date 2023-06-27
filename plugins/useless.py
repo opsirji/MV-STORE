c2d8fad5c9ad35457a12e8fc71a608e1feaef0b9
technical_aks <105633032+technicalaks@users.noreply.github.com>
2023-06-27 09:53:49 +0530

    Ahh 😅

--- plugins/useless.py
+++ plugins/useless.py
@@ -1,7 +1,7 @@
 from bot import Bot
 from pyrogram.types import Message
 from pyrogram import filters
-from config import ADMINS, BOT_STATS_TEXT, USER_REPLY_TEXT
+from config import ADMINS, BOT_STATS_TEXT
 from datetime import datetime
 from helper_func import get_readable_time
 
